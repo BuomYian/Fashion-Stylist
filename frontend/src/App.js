2c4986b783bd42cd98b75e7f3b55a31b217793b0
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-03-27 18:17:06 +0300

    added products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
+import data from "./data";
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Stylish</a>
       </header>
-      <main>List Products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.name}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>{product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.jsx
@@ -0,0 +1,50 @@
+const data = {
+  products: [
+    {
+      name: "Nike Slim Shirt",
+      slug: "nike-slim-shirt",
+      category: "Shirts",
+      image: "/images/photo-1562157873-818bc0726f68.avif",
+      price: 120,
+      countInStock: 10,
+      rating: 4.5,
+      numReview: 10,
+      description: "High quality shirt",
+    },
+    {
+      name: "Nike Shirt",
+      slug: "nike-shirt",
+      category: "Shirts",
+      image: "/images/photo-1434389677669-e08b4cac3105.avif",
+      price: 120,
+      countInStock: 10,
+      rating: 4.5,
+      numReview: 10,
+      description: "High quality shirt",
+    },
+    {
+      name: "Slim Shirt",
+      slug: "slim-shirt",
+      category: "Shirts",
+      image: "/images/photo-1585487000160-6ebcfceb0d03.avif",
+      price: 120,
+      countInStock: 10,
+      rating: 4.5,
+      numReview: 10,
+      description: "High quality shirt",
+    },
+    {
+      name: "Shirt",
+      slug: "shirt",
+      category: "Shirts",
+      image: "/images/photo-1552664199-fd31f7431a55.avif",
+      price: 120,
+      countInStock: 10,
+      rating: 4.5,
+      numReview: 10,
+      description: "High quality shirt",
+    },
+  ],
+};
+
+export default data;
