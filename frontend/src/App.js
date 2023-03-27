511a6beb7a15d3783edfc03ad1faa7b31f67fcd4
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-03-27 17:14:58 +0300

    create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Stylish</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
