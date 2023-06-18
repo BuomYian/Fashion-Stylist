cd2213fa254491fb5af3f9f9b6535ba489315048
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-06-18 14:15:39 +0300

    express

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,5 +1,6 @@
 // Importing necessary dependencies and models for handling ordersimport express from "express";
 import expressAsyncHandler from "express-async-handler";
+import express from "express";
 import Order from "../models/orderModel.js";
 import User from "../models/userModel.js";
 import Product from "../models/productModel.js";
